6987bfc43bad204375a339f801e370ae2b9eeff0
George Nicolaou <orionas.elite@gmail.com>
2023-06-28 22:08:24 +0300

    WORKING WITH LINE COLOR CHANGE

--- core/includes/assets/js/timeline-slider.js
+++ core/includes/assets/js/timeline-slider.js
@@ -1,11 +1,6 @@
-var timelineSlider; // Declare timelineSlider as a global variable
-
-jQuery(document).ready(function ($) {
-  timelineSlider = new Swiper(".swiper-container", {
-    slidesPerView: 1,
-    loop: true,
-    autoplay: timelineSliderSettings.autoplay, // Use the autoplay setting passed from PHP
-    autoplayDelay: timelineSliderSettings.autoplayDelay, // Use the autoplay delay setting passed from PHP
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize Swiper
+  var swiper = new Swiper(".swiper-container", {
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -14,74 +9,32 @@ jQuery(document).ready(function ($) {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    oon: {
+    on: {
       slideChange: function () {
-        var activeSlide = timelineSlider.activeIndex;
-        console.log();
-        var yearElements = document.querySelectorAll(
-          ".timeline-years .timeline-year"
-        );
-
-        // Remove the "active" class from all year elements
-        yearElements.forEach(function (yearElement) {
-          yearElement.classList.remove("active");
-        });
-
-        // Add the "active" class to the current year element
-        yearElements[activeSlide].classList.add("active");
+        var activeIndex = swiper[0].activeIndex;
+        setActiveTimelineItem(activeIndex);
       },
     },
   });
 
-  $(".timeline-year").on("click", function () {
-    var slideIndex = $(this).data("slide");
-    timelineSlider[0].slideTo(slideIndex + 1); // Navigate to the clicked slide
-  });
-});
-var timelineSlider; // Declare timelineSlider as a global variable
-
-jQuery(document).ready(function ($) {
-  timelineSlider = new Swiper(".swiper-container", {
-    slidesPerView: 1,
-    loop: true,
-    autoplay: timelineSliderSettings.autoplay, // Use the autoplay setting passed from PHP
-    autoplayDelay: timelineSliderSettings.autoplayDelay, // Use the autoplay delay setting passed from PHP
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  // Handle timeline item click
+  var timelineItems = document.querySelectorAll(".timeline-item");
+  timelineItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      var slideIndex = parseInt(this.getAttribute("data-slide"));
+      swiper[0].slideTo(slideIndex);
+      setActiveTimelineItem(slideIndex);
+    });
   });
 
-  $(".timeline-year").on("click", function () {
-    var slideIndex = $(this).data("slide");
-    timelineSlider[0].slideTo(slideIndex + 1); // Navigate to the clicked slide
-    // Remove "active" class from current active li
-    $(".timeline-year.active").removeClass("active");
-
-    // Set "active" class on the li with the corresponding data-slide value
-    $(".timeline-year[data-slide='" + (slideIndex + 1) + "']").addClass(
-      "active"
-    );
-
-    console.log("Current Index = " + (slideIndex + 1));
-    var totalBars = 9;
-    var currentIndex = slideIndex + 1;
-    // Calculate the range for 1/9 of the bars
-    var rangePercentage = 1 / totalBars;
-    var startIndex = Math.floor(
-      (currentIndex - 1) * rangePercentage * totalBars
-    );
-    var endIndex = Math.floor(currentIndex * rangePercentage * totalBars);
-
-    console.log("totalBars = " + totalBars);
-    console.log("startIndex = " + startIndex);
-    console.log("EnDIndex = " + endIndex);
-    $(".timeline-bar")
-      .slice(startIndex, endIndex)
-      .css("background-color", "black");
-  });
+  // Set active timeline item
+  function setActiveTimelineItem(index) {
+    timelineItems.forEach(function (item) {
+      item.classList.remove("active");
+    });
+    // Add active class to current and previous slides
+    for (var i = 0; i <= index; i++) {
+      timelineItems[i].classList.add("active");
+    }
+  }
 });
--- gn-horizontal-timeline.php
+++ gn-horizontal-timeline.php
@@ -64,16 +64,16 @@ function GNHORIZONT()
 function timeline_slider_enqueue_scripts()
 {
 	// Enqueue SwiperJS library
-	wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', array('jquery'), '9.4.1', false);
+	wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', array('jquery'), '9.4.1');
 
 	// Enqueue SwiperJS styles
 	wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css', array(), '9.4.1');
 
 	// Enqueue custom styles
-	wp_enqueue_style('timeline-slider-css', plugin_dir_url(__FILE__) . 'core/includes/assets/css/timeline-slider.css', array(), '1.0', false);
+	wp_enqueue_style('timeline-slider-css', plugin_dir_url(__FILE__) . 'core/includes/assets/css/timeline-slider.css', array(), '1.0');
 
 	// Enqueue custom script for initializing the slider
-	wp_enqueue_script('timeline-slider-script', plugin_dir_url(__FILE__) . 'core/includes/assets/js/timeline-slider.js', array('swiper-js'), '1.0', false);
+	wp_enqueue_script('timeline-slider-script', plugin_dir_url(__FILE__) . 'core/includes/assets/js/timeline-slider.js', array('swiper-js'), '1.0');
 
 	// Pass additional settings to the JavaScript file
 	$settings = array(
@@ -90,142 +90,110 @@ function timeline_slider_shortcode()
 {
 	ob_start(); ?>
 
-	<div class="timeline-slider-wrapper">
-		<div class="swiper-container">
-			<div class="swiper-wrapper">
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">1957</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
+	<div class="swiper-container">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide timeline-content">
+				<div class="image">
+					<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
 				</div>
-
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">1967</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
+				<div class="content">
+					<h2 class="year">1957</h2>
+					<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
+						διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
 				</div>
-
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">1989</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">1996</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">2001</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">2014</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">2017</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
-				</div>
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">2022</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
-				</div>
-				<div class="timeline-slide swiper-slide">
-					<div class="timeline-content">
-						<div class="image">
-							<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
-						</div>
-						<div class="content">
-							<h2 class="year">2023</h2>
-							<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
-								διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
-						</div>
-					</div>
-				</div>
-				<!-- Add other timeline-slide elements similarly -->
 			</div>
-			<div class="swiper-pagination"></div>
-			<div class="swiper-button-next"></div>
-			<div class="swiper-button-prev"></div>
+			<div class="swiper-slide timeline-content">
+				<div class="image">
+					<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
+				</div>
+				<div class="content">
+					<h2 class="year">1967</h2>
+					<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
+						διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
+				</div>
+			</div>
+
+			<div class="swiper-slide timeline-content">
+				<div class="image">
+					<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
+				</div>
+				<div class="content">
+					<h2 class="year">1996</h2>
+					<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
+						διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
+				</div>
+			</div>
+
+			<div class="swiper-slide timeline-content">
+				<div class="image">
+					<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
+				</div>
+				<div class="content">
+					<h2 class="year">2001</h2>
+					<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
+						διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
+				</div>
+			</div>
+
+			<div class="swiper-slide timeline-content">
+				<div class="image">
+					<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
+				</div>
+				<div class="content">
+					<h2 class="year">2014</h2>
+					<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
+						διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
+				</div>
+			</div>
+
+			<div class="swiper-slide timeline-content">
+				<div class="image">
+					<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
+				</div>
+				<div class="content">
+					<h2 class="year">2017</h2>
+					<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
+						διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
+				</div>
+			</div>
+
+			<div class="swiper-slide timeline-content">
+				<div class="image">
+					<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
+				</div>
+				<div class="content">
+					<h2 class="year">2022</h2>
+					<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
+						διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
+				</div>
+			</div>
+			<div class="swiper-slide timeline-content">
+				<div class="image">
+					<img src="/wp-content/uploads/2023/06/item1-.svg" alt="Slide Image">
+				</div>
+				<div class="content">
+					<h2 class="year">2023</h2>
+					<p>Ο Παρνασσός Στροβόλου έχει ήδη σελίδα στο Facebook και Instagram όπου δημοσιοποιούνται
+						διαρκώς τα νέα της ομάδας, διάφορες φωτογραφίες από αγώνες, προπονήσεις και εκδηλώσεις.</p>
+				</div>
+			</div>
 		</div>
-		<div class="timeline-bar">
-			<ul class="timeline-years">
-				<li class="timeline-year active" data-slide="0">1957</li>
-				<li class="timeline-year" data-slide="1">1967</li>
-				<li class="timeline-year" data-slide="2">1989</li>
-				<li class="timeline-year" data-slide="3">1996</li>
-				<li class="timeline-year" data-slide="4">2001</li>
-				<li class="timeline-year" data-slide="5">2014</li>
-				<li class="timeline-year" data-slide="6">2017</li>
-				<li class="timeline-year" data-slide="7">2022</li>
-				<li class="timeline-year" data-slide="8">2023</li>
-			</ul>
-		</div>
+	</div>
+	<div class="swiper-pagination"></div>
+	<div class="swiper-button-next"></div>
+	<div class="swiper-button-prev"></div>
+	</div>
+
+	<div class="timeline">
+		<div class="timeline-item active" data-slide="0">1957</div>
+		<div class="timeline-item" data-slide="1">1967</div>
+		<div class="timeline-item" data-slide="2">1996</div>
+		<div class="timeline-item" data-slide="3">2001</div>
+		<div class="timeline-item" data-slide="4">2014</div>
+		<div class="timeline-item" data-slide="5">2017</div>
+		<div class="timeline-item" data-slide="6">2022</div>
+		<div class="timeline-item" data-slide="7">2023</div>
+
 	</div>
 
 	<?php
